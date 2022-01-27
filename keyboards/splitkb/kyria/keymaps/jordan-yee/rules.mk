d9ebf6102f32bfdcb4dfbce02a74e35e263c4058
Jordan Yee <jyee117@gmail.com>
2022-01-27 13:20:23 -0700

    Enable link time optimization

--- keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
@@ -1,3 +1,5 @@
+LTO_ENABLE = yes           # Enable link time optimization to save space
+
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
