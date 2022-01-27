7c67a5cef52f8b650faadf2354fe27fb41f423ad
Jordan Yee <jyee117@gmail.com>
2022-01-27 13:43:35 -0700

    Disable unused features to save space

--- keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
@@ -1,4 +1,9 @@
 LTO_ENABLE = yes           # Enable link time optimization to save space
+CONSOLE_ENABLE = no        # Disable to save space
+COMMAND_ENABLE = no        # Disable to save space
+SPACE_CADET_ENABLE = no    # Disable to save space
+GRAVE_ESC_ENABLE = no      # Disable to save space
+MAGIC_ENABLE = no          # Disable to save space
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306      # Enables the use of OLED displays
