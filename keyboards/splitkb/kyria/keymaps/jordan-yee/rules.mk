a80e20241544bd781a1419415aa0508d5f23418a
Jordan Yee <jyee117@gmail.com>
2022-01-27 14:00:31 -0700

    Remove settings that duplicate keyboard settings

--- keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/rules.mk
@@ -1,12 +1,8 @@
-LTO_ENABLE = yes           # Enable link time optimization to save space
-CONSOLE_ENABLE = no        # Disable to save space
-COMMAND_ENABLE = no        # Disable to save space
-SPACE_CADET_ENABLE = no    # Disable to save space
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 GRAVE_ESC_ENABLE = no      # Disable to save space
 MAGIC_ENABLE = no          # Disable to save space
-
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306      # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 MOUSEKEY_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+OLED_ENABLE = yes
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+SPACE_CADET_ENABLE = no    # Disable to save space
