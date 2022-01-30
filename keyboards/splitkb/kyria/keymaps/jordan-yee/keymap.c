b30ef80373ab2f92036fba1e103c48ffd24894e7
Jordan Yee <jyee117@gmail.com>
2022-01-29 18:00:41 -0700

    Rebing 2u for testing

--- keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
@@ -61,17 +61,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | Ctrl/  |
  * |        |      |      |      |      |      |                              |      |      |      |      |      | Enter  |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |Numsym|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift/|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | [ {  |CapsLk|  |      |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift/|
  * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      | RCtrl  |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Adjust| LGUI | LAlt | Space| Nav  |  | RGUI | Space| Code |F-keys| Mute |
+ * `----------------------+------+------+------+------+------|  |      |------+------+------+------+----------------------'
+ *                        |Adjust| LGUI | LAlt | Space| Nav  |  |Numsym| Space| Code |F-keys| Mute |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                     KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                     KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, CTL_ENT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_CAPS, NUMSYM , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SFT_CTL,
-                                 ADJUST, KC_LGUI, KC_LALT, KC_SPC , NAV    , KC_RGUI, KC_SPC , CODE,  FKEYS ,KC_MUTE
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_CAPS, KC_SPC , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, SFT_CTL,
+                                 ADJUST, KC_LGUI, KC_LALT, KC_SPC , NAV    , KC_SPC , KC_SPC , CODE,  FKEYS ,KC_MUTE
     ),
 
 /*
