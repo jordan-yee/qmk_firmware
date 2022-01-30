e6b0e431a09e7245a2a84ce50b4268e28bd749c2
Jordan Yee <jyee117@gmail.com>
2022-01-29 17:23:47 -0700

    Enable debugging settings

--- keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
@@ -335,3 +335,22 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 #endif
+
+#ifdef CONSOLE_ENABLE
+void keyboard_post_init_user(void) {
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_matrix=true;
+    debug_keyboard=true;
+    debug_mouse=true;
+}
+#endif
+
+// Temporary debugging: identify matrix positions of keys
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // If console is enabled, it will print the matrix position and status of each key pressed
+    #ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+    #endif
+    return true;
+}
