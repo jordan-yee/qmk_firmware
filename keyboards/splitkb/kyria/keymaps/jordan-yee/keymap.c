9a1f49a0b7bea51284387d332f59f32331cccfca
Jordan Yee <jordan@GAMEMASTER-REBORN.localdomain>
2025-09-16 14:56:46 -0600

    Fix minux key name

--- keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jordan-yee/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                     KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC ,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                     KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_CAPS, XXXXXXX, KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,     MINS,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC, KC_CAPS, XXXXXXX, KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,  KC_MINS,
                                  ADJUST, ALTGUI , KC_LGUI, ALT_ENT , NAV   , NUMSYM , KC_SPC , CODE,  FKEYS ,KC_MUTE
     ),
 
